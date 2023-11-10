4d6ad8b73ab8d81faa5df2ded03414d10b25fbb0
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-10 10:40:23 -0800

    Write second test for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -10,5 +10,9 @@ describe ("AgeCalculator", () => {
 
   test("should add input to earthYears parameter of object", () => {
     expect(myAgeCalculator.earthYears).toEqual(10);
-  })
+  });
+
+  test("should calculate the age divided by .24 (to two decimal places) when calculateMercuryYears is called", () => {
+    expect(myAgeCalculator.calculateMercuryYears).toEqual(41.66)
+  });
 })
