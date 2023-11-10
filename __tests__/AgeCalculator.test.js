014150209c100d0376f552b5c19e95b2a9e16cd3
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-10 10:29:55 -0800

    Write first test for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -1,1 +1,14 @@
 import { AgeCalculator } from '../src/js/AgeCalculator.js';
+
+describe ("AgeCalculator", () => {
+
+  let myAgeCalculator;
+
+  beforeEach(() => {
+    myAgeCalculator = new AgeCalculator(10);
+  });
+
+  test("should add input to earthYears parameter of object", () => {
+    expect(myAgeCalculator.earthYears).toEqual(10);
+  })
+})
