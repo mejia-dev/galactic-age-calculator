ce6e2875d02489d24b068eda240fed4420285f4a
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-10 14:59:44 -0800

    WIP Commit while trying to get form working.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import AgeCalculator from './js/AgeCalculator.js';
+import { AgeCalculator } from './js/AgeCalculator.js';
+
+let myAgeCalculator;
 
 function enterAgeForm() {
   event.preventDefault();
 
-  // let myAgeCalculator = new AgeCalculator;
-
-  document.getElementById("outputDiv").innerText = null;
+  let age = parseInt(document.getElementById("age-input").value);
+  myAgeCalculator = new AgeCalculator(age);
+  // document.getElementById("planetDiv").innerText = null;
+  // Age on Mercury: ${myAgeCalculator.calculateMercuryYears()}
+  // Age on Venus: ${myAgeCalculator.calculateVenusYears()}
+  // Age on Mars: ${myAgeCalculator.calculateMarsYears()}
+  // Age on Jupiter: ${myAgeCalculator.calculateJupiterYears()}
   const pTag = document.createElement("p");
-  pTag.append(document.getElementById("text-input").value);
+  let earthYears = myAgeCalculator.earthYears;
+  console.log(earthYears.toString());
+
+  pTag.append(earthYears);
   document.getElementById("outputDiv").append(pTag);
 }
 
