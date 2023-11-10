65afb84612bcf257c70b9e316262243e09ac920d
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-10 11:11:51 -0800

    Write fourth test for AgeCalculator

--- __tests__/AgeCalculator.test.js
+++ __tests__/AgeCalculator.test.js
@@ -1,6 +1,6 @@
 import { AgeCalculator } from '../src/js/AgeCalculator.js';
 
-describe ("AgeCalculator", () => {
+describe("AgeCalculator", () => {
 
   let myAgeCalculator;
 
@@ -13,11 +13,14 @@ describe ("AgeCalculator", () => {
   });
 
   test("should calculate the age divided by .24 to two decimal places (not rounded) when calculateMercuryYears is called", () => {
-    expect(myAgeCalculator.calculateMercuryYears()).toEqual(41.66)
+    expect(myAgeCalculator.calculateMercuryYears()).toEqual(41.66);
   });
 
   test("should calculate the age divided by .62 to two decimal places (not rounded) when calculateVenusYears is called", () => {
-    expect(myAgeCalculator.calculateVenusYears()).toEqual(16.12)
+    expect(myAgeCalculator.calculateVenusYears()).toEqual(16.12);
   });
 
+  test("should calculate the age divided by 1.88 to two decimal places (not rounded) when calculateMarsYears is called", () => {
+    expect(myAgeCalculator.calculateMarsYears()).toEqual(5.31);
+  });
 })
