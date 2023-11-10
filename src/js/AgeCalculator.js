33e269817d83929751e8a94fcf1ea1ddc46be8d0
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-10 11:03:44 -0800

    Pass second test for AgeCalculator

--- src/js/AgeCalculator.js
+++ src/js/AgeCalculator.js
@@ -2,4 +2,10 @@ export class AgeCalculator {
   constructor(ageInput) {
     this.earthYears = ageInput;
   }
+
+  calculateMercuryYears() {
+    let mercuryMathString = (this.earthYears / 0.24).toString();
+    let mercuryYears = parseFloat(mercuryMathString.match(/^-?\d+(?:\.\d{0,2})?/)[0]);
+    return mercuryYears;
+  }
 }
